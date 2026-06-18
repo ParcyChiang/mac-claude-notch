10ded080741624b3cffbdc4488abbadb8734f3a8
蒋旻轩 <parcysyou@hotmail.com>
2026-06-18 14:53:27 +0800

    Initial Commit

---
+++ claude-notch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  claude-notch
+//
+//  Created by Ruby蒋 on 2026/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ claude-notch/claude_notchApp.swift
@@ -0,0 +1,17 @@
+//
+//  claude_notchApp.swift
+//  claude-notch
+//
+//  Created by Ruby蒋 on 2026/6/18.
+//
+
+import SwiftUI
+
+@main
+struct claude_notchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
