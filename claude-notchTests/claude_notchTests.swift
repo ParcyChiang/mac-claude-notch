dbbf496517d5182b8aaeded70749c74cd1013db5
蒋旻轩 <parcysyou@hotmail.com>
2026-06-18 15:19:19 +0800

    chore: add Unit Test target

---
+++ claude-notchTests/claude_notchTests.swift
@@ -0,0 +1,18 @@
+//
+//  claude_notchTests.swift
+//  claude-notchTests
+//
+//  Created by Ruby蒋 on 2026/6/18.
+//
+
+import Testing
+
+struct claude_notchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
